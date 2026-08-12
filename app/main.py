a20d267f196a5d9cc42663cfac8f9d481f17e6fd
Marcos Petry <marcosdpetry@gmail.com>
2026-08-12 16:12:49 -0300

    Allow local origins in CORS

--- app/main.py
+++ app/main.py
@@ -15,6 +15,8 @@ app.add_middleware(
     allow_origins=[
         "https://mdpconsultoria.com.br",
         "https://www.mdpconsultoria.com.br",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
     ],
     allow_credentials=True,
     allow_methods=["*"],
